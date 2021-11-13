4af96a7aa909ba1f808de03a148f1853b4e2b08a
aishwaryaaher <91543285+aishwaryaaher@users.noreply.github.com>
2021-11-13 10:09:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //for (  int i=10; i >= 5; i--)
+            //{
+            //    Console.WriteLine(i);
+                
+            //}
+            //Console.ReadLine();
+                               // 0 1  2  3   4  5
+            int[] LuckyNumbers = {2,4,18,20,14,16};
+            for (int i = 0; i < LuckyNumbers.Length; i++) 
+            {
+                Console.WriteLine(LuckyNumbers[i]);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
+
+/*
+
+1. digital marketing 20hrs free-
+2.Graphic design
+3.microsoft excel register login enrol ( PROMOCODE =FZFACT)
+ 4.data anylist
+5.Guide indexer job searching
+6.Resume Writing
+7.master in demand proffessional soft skills
+8..Effective skill
+ * 9. Seeling a skill
+ * 11.CS50
+
+*/
