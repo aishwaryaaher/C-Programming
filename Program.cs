d41cc5d39c59fcd1b125421d0c025546762369c2
aishwaryaaher <91543285+aishwaryaaher@users.noreply.github.com>
2021-11-13 10:13:39 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,60 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace _26_Constructors
+namespace ATMMachineProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // i can make  some change  to make code small
-
-            Book book1 = new Book("Harry Potter","JK Rowling",5000);
-            Book book2 = new Book("Lord Of The Rings", "Tolkein", 3000);
-            Book book3= new Book();
-            // i can also update the value of books
-             book2.title="The Habbits";
-            Console.WriteLine(book2.title);
-            Console.ReadLine();
-
-
-
-
-
-
-
-
-            //Book book1 = new Book();
-            ///*
-            //// using  parameters  
-            //Book book1 = new Book("Mike");
-            //*/
-
-            //book1.title = "Harry Potter";
-            //book1.author ="Jk Rowling";
-            //book1.pageNo = 4000;
-
-            //Book book2 = new Book(); 
+            int Amt = 10000, a, current, pin = 4040, pin1, pin2;
+            Console.WriteLine("Enter the pin ");
+            pin1 = int.Parse(Console.ReadLine());
            
-            ///* // using  parameters
-            //     Book book2 = new Book("John");
-            //*/
-            
-            //book2.title = "Lord Of The Rings";
-            //book2.author = "Tolkein";
-            //book2.pageNo = 4000;
+            if(!(pin==pin1))
+            {
+                Console.WriteLine("You Enter Wrong pin please try again>>>>>>>>>>>>>>>>");
+            }
+            else if()
+            {
+                Console.WriteLine(" ");
+            }
 
-            //Console.WriteLine(book1.title);
-            //Console.WriteLine(book1.author);
-            //Console.WriteLine(book1.pageNo);
+            else(pin == pin1)
+            {
+                Console.WriteLine("1.To Check Balance");
+                Console.WriteLine("2.To Withdraw Money");
+                Console.WriteLine("3.To deposite Money");
+                Console.WriteLine("4.To change pin ");
+                Console.WriteLine("Enter your Choice");
 
-            //Console.WriteLine("");
+                int ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("The current balance is in your account is" + Amt);
+                        break;
 
-            //Console.WriteLine(book2.title);
-            //Console.WriteLine(book2.author);
-            //Console.WriteLine(book2.pageNo);
 
-          
+                    case 2:
+                        Console.WriteLine("Enter the Amount to withdraw");
+                        {
+                            a = int.Parse(Console.ReadLine());
+                            if (Amt >= a)
+                            {
+                                if (a % 100 == 0)
+                                {
+                                    Console.WriteLine("Please collect the cash" + a);
+                                    current = Amt - a;
+                                    Console.WriteLine("The Current balance is now" + current);
+                                }
+                                else
+
+                                    Console.WriteLine("Please enter the amount to withdraw in the multiples of 100");
+
+                            }
+                            else
+
+                                Console.WriteLine("Your Account does not have sufficient balance");
+
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter the amount to be deposite");
+                        a = int.Parse(Console.ReadLine());
+                        current = Amt + a;
+                        Console.WriteLine(" The Current balance in the account is " + current);
+                        break;
+
+
+                    case 4:
+                        Console.WriteLine("Want to change your pin");
+                        Console.WriteLine("enter your Previous pin");
+                        int prepin = int.Parse(Console.ReadLine());
+                        if (prepin == pin)
+                        {
+                            Console.WriteLine("Enter your new pin");
+                            pin2 = int.Parse(Console.ReadLine());
+                            pin1 = pin2;
+                            Console.WriteLine("Yor pin is changed");
+
+                        }
+                        else Console.WriteLine("Enter your correct pin ");
+                        break;
+
+                    default:
+                        Console.WriteLine("Pleae select correct option");
+                        break;
+
+                }
+            }
+
+           
         }
     }
 }
